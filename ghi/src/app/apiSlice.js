91ef0fba48cf8b7f75bd910238867aa9c1a79234
Brandon Tiernay <brandontiernay@gmail.com>
2023-08-30 15:40:51 -0400

    complete logout functionality

--- ghi/src/app/apiSlice.js
+++ ghi/src/app/apiSlice.js
@@ -5,10 +5,12 @@ export const memeApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: process.env.REACT_APP_API_HOST,
   }),
+
   endpoints: (builder) => ({
     getAllMemes: builder.query({
       query: () => "/api/memes",
     }),
+
     login: builder.mutation({
       query: (input) => {
         const formData = new FormData();
@@ -22,12 +24,15 @@ export const memeApi = createApi({
           credentials: "include",
         };
       },
+      invalidatesTags: ["Token"],
     }),
+
     getToken: builder.query({
       query: () => ({
         url: "/token",
         credentials: "include",
       }),
+      transformResponse: (response) => response?.account || null,
       providesTags: ["Token"],
     }),
 
@@ -40,9 +45,26 @@ export const memeApi = createApi({
           credentials: "include",
         };
       },
+      invalidatesTags: ["Token"],
+    }),
+
+    logout: builder.mutation({
+      query: () => {
+        return {
+          url: "/token",
+          method: "DELETE",
+          credentials: "include",
+        };
+      },
+      invalidatesTags: ["Token"],
     }),
   }),
 });
 
-export const { useGetAllMemesQuery, useLoginMutation, useSignUpMutation } =
-  memeApi;
+export const {
+  useGetAllMemesQuery,
+  useLoginMutation,
+  useSignUpMutation,
+  useLogoutMutation,
+  useGetTokenQuery,
+} = memeApi;
--- ghi/src/components/Nav.jsx
+++ ghi/src/components/Nav.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { useLogoutMutation, useGetTokenQuery } from "../app/apiSlice";
 
 export const Nav = () => {
+  const [logout] = useLogoutMutation();
+  const { data: token } = useGetTokenQuery();
+
   return (
     <nav
       className="navbar navbar-expand-lg bg-dark border-bottom border-body"
@@ -17,37 +21,53 @@ export const Nav = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         ></button>
+
         <ul className="navbar-nav">
           <li className="nav-item">
             <NavLink className="nav-link" to="/">
               MemeReactor
             </NavLink>
           </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/generator">
-              Create Meme
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/profile">
-              User Profile
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/signup">
-              Sign Up
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/login">
-              Login
-            </NavLink>
-          </li>
-          <li className="nav-item">
-            <NavLink className="nav-link" to="/">
-              Logout
-            </NavLink>
-          </li>
+
+          {token && (
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/generator">
+                Create Meme
+              </NavLink>
+            </li>
+          )}
+
+          {token && (
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/profile">
+                User Profile
+              </NavLink>
+            </li>
+          )}
+
+          {!token && (
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/signup">
+                Sign Up
+              </NavLink>
+            </li>
+          )}
+
+          {!token && (
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/login">
+                Login
+              </NavLink>
+            </li>
+          )}
+
+          {token && (
+            <li className="nav-item">
+              <Link onClick={logout} className="nav-link" to="/">
+                Logout
+              </Link>
+            </li>
+          )}
         </ul>
       </div>
     </nav>
