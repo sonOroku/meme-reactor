fae7b15a8124634c6db0d210661e7b486397cead
Brandon Tiernay <brandontiernay@gmail.com>
2023-09-19 18:39:57 -0400

    disable eslint on Login, Signup, and LikedButtons

--- ghi/src/pages/Login.jsx
+++ ghi/src/pages/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState, useEffect } from "react";
 import { useLoginMutation } from "../app/apiSlice";
 import { useNavigate } from "react-router-dom";
--- ghi/src/pages/SignUp.jsx
+++ ghi/src/pages/SignUp.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import React, { useState, useEffect } from "react";
 import { useSignUpMutation } from "../app/apiSlice";
 import { useNavigate } from "react-router-dom";
