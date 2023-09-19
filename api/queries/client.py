166928b1f15a62148abe7eaa24a6e5a0e9531189
rcrandon <randallcrandon@gmail.com>
2023-09-19 19:23:09 -0400

    Added check to ensure DB_NAME is a string before initializing
    Added a check to ensure that DB_NAME is a string before initializing the database. If DB_NAME is None or not a string, the program should raise a clear error.

--- api/queries/client.py
+++ api/queries/client.py
@@ -4,6 +4,9 @@ import os
 DATABASE_URL = os.environ.get("DATABASE_URL")
 DB_NAME = os.environ.get("DB_NAME")
 
+if not isinstance(DB_NAME, str):
+    raise ValueError("DB_NAME must be a string.")
+
 client = MongoClient(DATABASE_URL)
 db = client[DB_NAME]
 
